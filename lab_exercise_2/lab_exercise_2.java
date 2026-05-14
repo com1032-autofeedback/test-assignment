1889702eeaa311ee195903162cb99d76a8311a69
Angelo Diaz <sd01753@surrey.ac.uk>
2026-05-14 15:04:35 +0100

    test comparison 1

--- lab_exercise_2/lab_exercise_2.java
+++ lab_exercise_2/lab_exercise_2.java
@@ -1,32 +1,17 @@
 package lab_exercise_2;
 public class lab_exercise_2 { 
     
-    public static void main(String[] args) {
-
-        // Getting the sizes using the hint
-        int s_size = Short.SIZE;
-
-        
-        
-        // int c_size = Char.SIZE; // this gave me an error so I checked stackoverflow, it's Character
-        int c_size = Character.SIZE;
-        
-        // Printing them out. 
-        // Wait, Short.SIZE prints 16 instead of 2. I think it gives bits instead of bytes?
-        // I have to divide by 8 to get the actual bytes. 
-        
-        System.out.println("Size of short: " + s_size / 8 + " bytes");
-        System.out.println("Size of char: " + c_size / 8 + " bytes");
-        System.out.println("Size of int: " + Integer.SIZE / 8 + " bytes ");
-        
-        // I'll just do the rest directly in the print statement
-        System.out.println("Size of long: " + Long.SIZE + " bytes"); // Oops, forgot to divide by 8
-        System.out.println("Size of float: " + Float.SIZE / 8 + " bytes");
-        
-        double dbl_size = Double.SIZE;
-        System.out.println("Size of double: " + (dbl_size / 8) + " bytes");
-        
-    }
+	public class Main {
+	    public static void main(String[] args) {
+	        java.io.File f = new java.io.File("file.txt");
+	        java.util.Scanner s = new java.util.Scanner(f);
+	        int i = 1;
+	        while(s.hasNextLine()) {
+	            System.out.println(i + s.nextLine());
+	            i++;
+	        }
+	    }
+	}
 }
 
 
